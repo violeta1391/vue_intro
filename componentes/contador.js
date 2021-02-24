0566a806c99d82e3a92760047f4264ae873df081
Violeta Rodriguez <violeta.rodriguez@globant.com>
2021-02-24 16:32:51 -0300

    componente contador

--- componentes/contador.js
+++ componentes/contador.js
@@ -1,15 +1,15 @@
-Vue.component('saludo', {
+Vue.component('contador', {
     template:
     `
     <div class="p-4 m-5 text-center bg-dark text-white text-center">
-    <h1>{{saludo}}</h1> 
-    <h3>asd</h3>   
+    <h1>{{numero}}</h1> 
+    <button class="btn btn-primary" @click="numero++"> + </button>
     </div>
     
     `, 
     data(){
         return {
-            saludo: 'hola desde vue'
+            numero: 0
         }
     }
 })
