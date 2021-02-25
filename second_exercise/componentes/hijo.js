9cdb3d5529a9c5b20fcc93eecd5abaf8270d20dc
Violeta <vio.ayelen@gmail.com>
2021-02-25 16:17:15 -0300

    comunicacion entre componentes

--- second_exercise/componentes/hijo.js
+++ second_exercise/componentes/hijo.js
@@ -3,8 +3,18 @@ Vue.component('hijo', {
     `
     <div class="p-4 m-5 text-center bg-success text-center">
     <h2>Componente hijo: {{numero}}</h2>     
+    <h4>Nombre: {{nombre}}</h4>
+    
     </div>
     
     `, 
-    props: ['numero']
+    props: ['numero'],
+    data() {
+        return {
+            nombre: 'violeta'
+        }
+    },
+    mounted(){
+        this.$emit('nombreHijo', this.nombre)
+    }
 })
--- second_exercise/componentes/padre.js
+++ second_exercise/componentes/padre.js
@@ -4,13 +4,15 @@ Vue.component('padre', {
     <div class="p-4 m-5 text-center bg-dark text-white text-center">
     <h1>Componente padre: {{numeroPadre}}</h1>
     <button class="btn btn-primary" @click="numeroPadre++"> + </button>     
-    <hijo :numero="numeroPadre"></hijo>
+    <hijo :numero="numeroPadre" @nombreHijo="nombrePadre =$event"></hijo>
+    {{nombrePadre}}
     </div>
     
     `, 
     data(){
         return {
-            numeroPadre: 0
+            numeroPadre: 0,
+            nombrePadre: " "
         }
     }
 })
